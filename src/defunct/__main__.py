3962cc0d2bb8ebfec9200e1995798b3e9ca1773e
George Clayton Bennett <george.bennett@memphistn.gov>
2025-04-30 11:19:47 -0500

    git rm --cached

--- src/defunct/__main__.py
+++
@@ -1,5 +0,0 @@
-#from libs.Api import PyEds2
-import src.get_rjn_auth_token
-from env import Env 
-if __name__ == "__main__":
-    src.get_rjn_auth_token.call_curl_script(username =  Env.get_username(),password =  Env.get_password())
