c3edad67242c7e21ada420687d373d2a9d6d9470
George Clayton Bennett <george.bennett@memphistn.gov>
2025-05-02 13:44:04 -0500

    stiles

--- culprit.ps1
+++
@@ -1,7 +0,0 @@
-Get-ChildItem -Recurse -Include *.py -ErrorAction SilentlyContinue | ForEach-Object {
-    try {
-        Get-Content $_.FullName -Encoding UTF8 | Out-Null
-    } catch {
-        Write-Host "Problematic file: $($_.FullName)"
-    }
-}
