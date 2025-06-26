408d718ff97bf9c97bd1a01943310d4e4d3ca927
George Clayton Bennett <GEORGE.BENNETT@memphistn.gov>
2025-06-25 23:57:24 -0500

    a challenger will rise

--- src/pipeline/plotbuffer.py
+++ src/pipeline/plotbuffer.py
@@ -1,7 +1,7 @@
 # src/pipeline/plotbuffer.py
 from collections import defaultdict
 
-class PlotBuffer:
+class PlotBuffer_dead:
     def __init__(self, max_length=100):
         self.max_length = max_length
         self.data = defaultdict(lambda: {"x": [], "y": []})
@@ -13,6 +13,23 @@ class PlotBuffer:
         if len(series["x"]) > self.max_length:
             series["x"].pop(0)
             series["y"].pop(0)
+
+    def get_all(self):
+        return self.data
+
+class PlotBuffer:
+    def __init__(self, max_points=100):
+        from collections import defaultdict
+        self.data = defaultdict(lambda: {"x": [], "y": []})
+        self.max_points = max_points
+
+    def append(self, label, x, y):
+        self.data[label]["x"].append(x)
+        self.data[label]["y"].append(y)
+
+        if len(self.data[label]["x"]) > self.max_points:
+            self.data[label]["x"].pop(0)
+            self.data[label]["y"].pop(0)
 
     def get_all(self):
         return self.data
